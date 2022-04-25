6dd3b5791eb05e5a39a66506416bdb01eb683d56
Sangwoo Joo <nooni85@naver.com>
2022-04-25 21:20:47 +0900

    React example

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  }
 }
 
 export default App;
